91823b33f27e5491bd8d92fedf2cbc2e3ff7186c
Adolpho Cavalcanti <adolpho.nascimento@compasso.com.br>
2022-11-14 15:27:00 -0300

    Adicionando title ao site

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+        <title>Site | Adolpho Cavalcanti</title>
       </Head>
       <body className="bg-black">
         <Main />
